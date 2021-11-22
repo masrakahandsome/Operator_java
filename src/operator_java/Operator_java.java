968e12bb9f473635d2a52d198a1f3993a1ae6f1b
rakawir@gmail.com <rakawir@mail.com>
2021-11-22 20:17:17 +0800

    first commit

---
+++ src/operator_java/Operator_java.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operator_java;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Operator_java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nama = " I Putu Raka Wiratma";
+        int semester;
+        semester = 4 + 8 - 2 / 1 * 2;
+        
+        System.out.println("Nama saya :"+ nama);
+        System.out.println("Saya Semester :" + semester);
+    }
+    
+    
+}
